2538ba08276a75b7c109e91e58c190882293a30d
Reproog <etanhunt0@gmail.com>
2024-06-11 08:33:35 +0200

    Update

--- nob.c
+++ nob.c
@@ -9,11 +9,11 @@ int main(int argc, char **argv)
     Nob_Cmd cmd = {0};          // SRC FILE
     
     nob_cmd_append(&cmd, "gcc", "src/main.c", "-ggdb");
-    nob_cmd_append(&cmd, "-I", "./sqlite-src/sqlite3-src/", "-I", "./raylib-src/raylib-5.0_linux_amd64/include/");
+    nob_cmd_append(&cmd, "-I", "./raylib-src/raylib-5.0_linux_amd64/include/");
     nob_cmd_append(&cmd, "-L", "./raylib-src/raylib-5.0_linux_amd64/lib/", "-L", "./raylib-src/old-raygui-src/", "-l:libraylib.a", "-l:raygui.so", "-lm");
 
     // Main Exec File
-    nob_cmd_append(&cmd, "-o", "./bin/main.c");
+    nob_cmd_append(&cmd, "-o", "./main");
 
     if (!nob_cmd_run_sync(cmd)) return -1;
      
