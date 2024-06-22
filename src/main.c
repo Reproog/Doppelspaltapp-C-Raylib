af551d9bb87e6caeb5812f6662b8e3c3134e9ea8
Reproog <etanhunt0@gmail.com>
2024-06-22 14:27:42 +0200

    circle Smoother

--- src/main.c
+++ src/main.c
@@ -120,8 +120,11 @@ void DrawKugelwelle(int x, int y, int breite, int hoehe, AppState* state, Color 
     for (int i = 0; i * state->lambda + s0 < breite; i++) {
         int radius = i * state->lambda + s0;
 
+        Vector2 centerVec = {x, y};
         if (radius > 0) {
-            DrawCircleLines(x, y, radius, color);
+            DrawCircleSectorLines(centerVec, radius, -90.0f, 90.0f, 100, color);
+            //DrawCircleLines(x, y, radius, color);
+
         }
     }
 }
